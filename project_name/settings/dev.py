be7729d6bc0a7ca75e2bb53346782011c2810e58
Yannik Messerli <ymesserl@cisco.com>
2014-02-09 23:38:42 -0800

    Remove debug bar related information

--- project_name/settings/dev.py
+++ project_name/settings/dev.py
@@ -53,23 +53,3 @@ CELERY_ALWAYS_EAGER = True
 # See: http://docs.celeryproject.org/en/latest/configuration.html#celery-eager-propagates-exceptions
 CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
 ########## END CELERY CONFIGURATION
-
-
-########## TOOLBAR CONFIGURATION
-# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
-
-# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
-INTERNAL_IPS = ('127.0.0.1',)
-
-# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-}
-########## END TOOLBAR CONFIGURATION
